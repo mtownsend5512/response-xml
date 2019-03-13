76607ecbbef60a084af034cfdeb7735aa4969d43
Mark <mtownsend5512@gmail.com>
2019-03-12 21:31:51 -0500

    Fixed incompatibility issues with Lumen
    Prior to this release, Lumen was incompatible due to Lumen's usage of
    ``Laravel\Lumen\Http\ResponseFactory`` instead of Laravel's native
    response factory class. This release allows Lumen applications to use
    this package's xml capabilities. 🎉

--- src/Providers/ResponseXmlServiceProvider.php
+++ src/Providers/ResponseXmlServiceProvider.php
@@ -3,7 +3,6 @@
 namespace Mtownsend\ResponseXml\Providers;
 
 use Illuminate\Container\Container;
-use Illuminate\Routing\ResponseFactory as Response;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Spatie\ArrayToXml\ArrayToXml;
@@ -27,7 +26,16 @@ class ResponseXmlServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Response::macro('xml', function ($xml, $status = 200, array $headers = [], $xmlRoot = 'response') {
+        $this->isLumen() ? $this->loadLumenResponseMacros() : $this->loadLaravelResponseMacros();
+    }
+
+    /**
+     * If the application is Laravel, load Laravel's response factory and macro the xml methods
+     *
+     */
+    protected function loadLaravelResponseMacros()
+    {
+        \Illuminate\Routing\ResponseFactory::macro('xml', function ($xml, $status = 200, array $headers = [], $xmlRoot = 'response') {
             if (is_array($xml)) {
                 $xml = ArrayToXml::convert($xml, $xmlRoot);
             } elseif (is_object($xml) && method_exists($xml, 'toArray')) {
@@ -40,10 +48,10 @@ class ResponseXmlServiceProvider extends ServiceProvider
             if (!isset($headers['Content-Type'])) {
                 $headers = array_merge($headers, ['Content-Type' => 'application/xml']);
             }
-            return Response::make($xml, $status, $headers);
+            return \Illuminate\Routing\ResponseFactory::make($xml, $status, $headers);
         });
 
-        Response::macro('preferredFormat', function ($data, $status = 200, array $headers = [], $xmlRoot = 'response') {
+        \Illuminate\Routing\ResponseFactory::macro('preferredFormat', function ($data, $status = 200, array $headers = [], $xmlRoot = 'response') {
             $request = Container::getInstance()->make('request');
             if (Str::contains($request->headers->get('Accept'), 'xml')) {
                 return $this->xml($data, $status, array_merge($headers, ['Content-Type' => $request->headers->get('Accept')]), $xmlRoot);
@@ -51,5 +59,45 @@ class ResponseXmlServiceProvider extends ServiceProvider
                 return $this->json($data, $status, array_merge($headers, ['Content-Type' => $request->headers->get('Accept')]));
             }
         });
+    }
+
+    /**
+     * If the application is Lumen, load Lumens's response factory and macro the xml methods
+     *
+     */
+    protected function loadLumenResponseMacros()
+    {
+        \Laravel\Lumen\Http\ResponseFactory::macro('xml', function ($xml, $status = 200, array $headers = [], $xmlRoot = 'response') {
+            if (is_array($xml)) {
+                $xml = ArrayToXml::convert($xml, $xmlRoot);
+            } elseif (is_object($xml) && method_exists($xml, 'toArray')) {
+                $xml = ArrayToXml::convert($xml->toArray(), $xmlRoot);
+            } elseif (is_string($xml)) {
+                $xml = $xml;
+            } else {
+                $xml = '';
+            }
+            if (!isset($headers['Content-Type'])) {
+                $headers = array_merge($headers, ['Content-Type' => 'application/xml']);
+            }
+            return \Laravel\Lumen\Http\ResponseFactory::make($xml, $status, $headers);
+        });
+
+        \Laravel\Lumen\Http\ResponseFactory::macro('preferredFormat', function ($data, $status = 200, array $headers = [], $xmlRoot = 'response') {
+            $request = Container::getInstance()->make('request');
+            if (Str::contains($request->headers->get('Accept'), 'xml')) {
+                return $this->xml($data, $status, array_merge($headers, ['Content-Type' => $request->headers->get('Accept')]), $xmlRoot);
+            } else {
+                return $this->json($data, $status, array_merge($headers, ['Content-Type' => $request->headers->get('Accept')]));
+            }
+        });
+    }
+
+    protected function isLumen()
+    {
+        if (!isset($this->app)) {
+            return false;
+        }
+        return Str::contains($this->app->version(), 'Lumen');
     }
 }
